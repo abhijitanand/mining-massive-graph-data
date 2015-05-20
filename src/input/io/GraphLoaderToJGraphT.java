445daa239fde1d8df38234658ab7a35ed1a7f842
Avishek Anand <avishekanand@gmail.com>
2015-05-20 17:00:34 +0200

    added diameter experiment

---
+++ src/input/io/GraphLoaderToJGraphT.java
@@ -0,0 +1,117 @@
+package input.io;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.zip.GZIPInputStream;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.AsUnweightedDirectedGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ *
+ * @author avishekanand
+ */
+public class GraphLoaderToJGraphT {
+    
+    BufferedReader _input;
+    
+    DirectedGraph<Integer, DefaultEdge> webGraph;
+    
+    public GraphLoaderToJGraphT(String input) throws IOException{
+        _input = getReader(input);
+        
+        int nodes = 0;
+        int edges = 0;
+        
+        //first line contians node and edge information
+        if(_input.ready()){
+            String[] line = _input.readLine().split("\\s+");
+            
+            nodes = Integer.parseInt(line[3]);
+            edges = Integer.parseInt(line[6]);
+            
+            System.out.println("Nodes : " + nodes + " , edges : " + edges);
+        }
+        
+        // create a graph based on URL objects
+        webGraph = constructDirectedWebGraph(_input, nodes);
+    }
+    
+    public BufferedReader getReader(String inputFile) throws FileNotFoundException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader (
+                                                new GZIPInputStream(new FileInputStream(inputFile))));
+        
+        return br;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        GraphLoaderToJGraphT graphLoader = new GraphLoaderToJGraphT("/Users/avishekanand/research/data/de-yr-graphs/de-2000.gz");
+        
+        System.out.println(graphLoader.webGraph.toString());
+        
+        
+    }
+
+    public DirectedGraph getWebGraph(){
+        return webGraph;
+    }
+            
+            
+    private DirectedGraph<Integer, DefaultEdge> constructDirectedWebGraph(BufferedReader _input, int nodes) throws IOException {
+        DirectedGraph<Integer, DefaultEdge> g =
+            new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+        
+        ArrayList<Integer>[] adjacencyLists = new ArrayList[nodes];
+
+        int[] nodeIDs = new int[nodes];
+        
+        try {
+            int index = 0;
+            // add vertices
+            while (_input.ready()) {                
+                String[] list = _input.readLine().split("\\s+");
+                
+                int nodeID = Integer.parseInt(list[0]);
+                g.addVertex(nodeID);
+                
+                adjacencyLists[index] = new ArrayList<>();
+                nodeIDs[index] = nodeID;
+                
+                //cases where a node doesnt have edges
+                if (list.length <= 1){
+                    continue;
+                }
+                
+                for (int i = 1; i < list.length; i++){
+                    adjacencyLists[index].add(Integer.parseInt(list[i]));
+                }
+                index++;
+            }
+            
+
+            // add edges to create linking structure
+            for (int i = 0; i < adjacencyLists.length; i++) {
+                int source = nodeIDs[i];
+                
+                if(adjacencyLists[i] == null || adjacencyLists[i].isEmpty()){
+                    continue;
+                }
+                
+                for (int target : adjacencyLists[i]){
+                    g.addEdge(source, target);
+                }
+            }
+            
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return g;
+    }
+}
